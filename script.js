ff2be367c491cd3d0ccbc7460bccc5d973bd03c8
Priti Rai <138751304+Pritiprty@users.noreply.github.com>
2024-02-11 12:25:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const extendHex = (shortHex) => {
-  // write your code here
+
+function extendHex(shortHex) {
+    // Remove the # symbol if present
+    shortHex = shortHex.replace("#", "");
+    
+    // If shortHex has 3 characters, extend each character to repeat
+    if (shortHex.length === 3) {
+        shortHex = shortHex.split("").map(function(char) {
+            return char + char;
+        }).join("");
+    }
+    
+    // Construct the full hex code
+    return "#" + shortHex.toUpperCase(); // Ensure uppercase output
 };
 
 // Do not change the code below.
